a31c398ede31771fbce150fc61887d59773c7660
Eetu Pasanen <eetu.pasanen@hotmail.com>
2021-12-09 15:21:48 +0200

    Add cors

--- routes/locations.js
+++ routes/locations.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const router = express.Router();
 
 router.use(express.json());
+router.use(cors());
+// router.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   next();
+// });
+
 nextID = 3;
 
 const data = [
