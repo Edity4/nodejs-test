623c067471d7a641534a64a203728d30d7140d38
Eetu Pasanen <eetu.pasanen@hotmail.com>
2021-12-09 11:10:23 +0200

    Add customer test

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 
+const data = [
+  { id: 1, name: "Jack" },
+  { id: 2, name: "Ashley" },
+  { id: 3, name: "Garrus" },
+  { id: 4, name: "Liara" },
+];
+
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.send("Hello World!!");
+});
+
+app.get("/customers", (req, res) => {
+  res.json(data);
 });
 
 app.listen(port, () => {
