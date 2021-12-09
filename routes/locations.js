d9760ba740145312f58ad61af14a97dbc1424095
Eetu Pasanen <eetu.pasanen@hotmail.com>
2021-12-09 14:32:16 +0200

    Add HTTP GET, POST and DELETE

--- routes/locations.js
+++ routes/locations.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.json([
+    { id: 1, latitude: 60, longitude: 60 },
+    { id: 2, latitude: 80, longitude: 40 },
+  ]);
+});
+
 router.get("/1", (req, res) => {
   res.json({ id: 1, latitude: 60, longitude: 60 });
+});
+
+router.get("/2", (req, res) => {
+  res.json({ id: 2, latitude: 80, longitude: 40 });
+});
+
+router.post("/", (req, res) => {
+  res.send("Adding new location");
+});
+
+router.delete("/1", (req, res) => {
+  res.send("Delete location with id 1");
 });
 
 module.exports = router;
